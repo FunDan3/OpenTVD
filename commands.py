1543ffeefaea195c11500841c25d49054d09f0b9
Fun_Dan3 <dfr34560@gmail.com>
2024-03-03 08:39:02 +0300

    Added start messages

--- commands.py
+++ commands.py
@@ -1,4 +1,8 @@
 def register(bot, language_pack):
-	@bot.message_handler()
-	def anything(message):
-		bot.send_message(message.chat.id, f"{message.chat.id}")
+	@bot.message_handler(commands = ["license"])
+	def license(message):
+		bot.send_message(message.chat.id, language_pack["LicenseText"])
+
+	@bot.message_handler(commands = ["start", "help"])
+	def start(message):
+		bot.send_message(message.chat.id, language_pack["StartText"])
